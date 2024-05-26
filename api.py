2c6606676eba6e851021fcb9e17ea63cae2c11c6
unknown <kiversonlc@gmail.com>
2024-05-26 18:43:44 +0800

    Utilize MySQL and add get staff fucntion

--- api.py
+++ api.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify, make_response
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+# Required configurations
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "1234"
+app.config["MYSQL_DB"] = "mydb"
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+
+mysql = MySQL(app)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/mydb", methods=["GET"])
+def get_actors():
+    cur = mysql.connection.cursor()
+    query = "SELECT * FROM staff"
+    cur.execute(query)
+    data = cur.fetchall()
+    cur.close()
+    return make_response(jsonify(data), 200)
 
 if __name__ == "__main__":
     app.run(debug=True)
